1fa7a2550520c87f8aa9a609edd9e62d9aa40bb6
Hugo Santos Lisboa <hugohotelmobiles@gmail.com>
2024-12-04 12:06:47 -0300

    first

---
+++ index.js
@@ -0,0 +1,54 @@
+// main.js
+const { app, BrowserWindow, ipcMain } = require('electron')
+const { spawn } = require('child_process')
+const path = require('path')
+
+function createMainWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      webviewTag: true  // Importante para habilitar WebView
+    }
+  })
+
+  mainWindow.loadFile('index.html')
+}
+
+// Método 1: WebView Integration
+ipcMain.on('launch-webview-nautilus', (event) => {
+  const nautilus = spawn('nautilus', ['--browser'])
+  nautilus.stdout.on('data', (data) => {
+    event.reply('nautilus-output', data.toString())
+  })
+})
+
+// Método 2: X11 Bridging (pseudo-código, requer biblioteca específica)
+ipcMain.on('launch-x11-bridge', async (event) => {
+  try {
+    // Exemplo conceitual de como poderia ser um bridge de X11
+    const x11Bridge = await import('electron-x11-bridge')
+    const window = x11Bridge.embedX11App('nautilus')
+    event.reply('x11-bridge-ready', window.id)
+  } catch (error) {
+    event.reply('x11-bridge-error', error.message)
+  }
+})
+
+// Método 3: iframe com xpra
+ipcMain.on('launch-xpra-iframe', (event) => {
+  // Comando para iniciar sessão Xpra
+  const xpraProcess = spawn('xpra', [
+    'start', 
+    '--bind-tcp=0.0.0.0:10000', 
+    '--start-child=nautilus'
+  ])
+
+  xpraProcess.stdout.on('data', (data) => {
+    event.reply('xpra-status', data.toString())
+  })
+})
+
+app.whenReady().then(createMainWindow)
