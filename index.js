2ad6b2484944ce4c6cef8d3847fb2f36e710149f
Hugo Santos Lisboa <hugohotelmobiles@gmail.com>
2024-12-04 12:12:45 -0300

    taskbar

--- index.js
+++ index.js
@@ -1,54 +1,76 @@
-// main.js
 const { app, BrowserWindow, ipcMain } = require('electron')
-const { spawn } = require('child_process')
-const path = require('path')
+const { exec } = require('child_process')
+const x11 = require('x11')
 
-function createMainWindow() {
+class WindowManager {
+  constructor() {
+    this.activeWindows = new Map()
+  }
+
+  async initialize() {
+    return new Promise((resolve, reject) => {
+      x11.createClient((err, display) => {
+        if (err) reject(err)
+        this.X = display.client
+        this.root = display.screen[0].root
+        this.setupWindowTracking()
+        resolve()
+      })
+    })
+  }
+
+  setupWindowTracking() {
+    this.X.ListWindows(this.root, (windows) => {
+      windows.forEach(window => this.trackWindow(window))
+    })
+
+    // Eventos de criação/destruição de janelas
+    this.X.on('MapNotify', (ev) => this.trackWindow(ev.window))
+    this.X.on('UnmapNotify', (ev) => this.removeWindow(ev.window))
+  }
+
+  trackWindow(windowId) {
+    this.X.GetWindowAttributes(windowId, (attrs) => {
+      this.X.GetWindowName(windowId, (name) => {
+        this.activeWindows.set(windowId, {
+          id: windowId,
+          name: name || 'Unnamed Window',
+          class: attrs.class || 'Unknown'
+        })
+      })
+    })
+  }
+
+  removeWindow(windowId) {
+    this.activeWindows.delete(windowId)
+  }
+
+  getActiveWindows() {
+    return Array.from(this.activeWindows.values())
+  }
+}
+
+function createMainWindow(windowManager) {
   const mainWindow = new BrowserWindow({
-    width: 1200,
-    height: 800,
+    width: 300,
+    height: 600,
     webPreferences: {
       nodeIntegration: true,
-      contextIsolation: false,
-      webviewTag: true  // Importante para habilitar WebView
+      contextIsolation: false
     }
   })
 
   mainWindow.loadFile('index.html')
+
+  // Atualizar lista de janelas periodicamente
+  setInterval(() => {
+    const windows = windowManager.getActiveWindows()
+    mainWindow.webContents.send('update-windows', windows)
+  }, 2000)
 }
 
-// Método 1: WebView Integration
-ipcMain.on('launch-webview-thunar', (event) => {
-  const thunar = spawn('thunar', ['--browser'])
-  thunar.stdout.on('data', (data) => {
-    event.reply('thunar-output', data.toString())
-  })
+app.whenReady().then(async () => {
+  const windowManager = new WindowManager()
+  await windowManager.initialize()
+  createMainWindow(windowManager)
 })
-
-// Método 2: X11 Bridging (pseudo-código, requer biblioteca específica)
-ipcMain.on('launch-x11-bridge', async (event) => {
-  try {
-    // Exemplo conceitual de como poderia ser um bridge de X11
-    const x11Bridge = await import('electron-x11-bridge')
-    const window = x11Bridge.embedX11App('thunar')
-    event.reply('x11-bridge-ready', window.id)
-  } catch (error) {
-    event.reply('x11-bridge-error', error.message)
-  }
-})
-
-// Método 3: iframe com xpra
-ipcMain.on('launch-xpra-iframe', (event) => {
-  // Comando para iniciar sessão Xpra
-  const xpraProcess = spawn('xpra', [
-    'start', 
-    '--bind-tcp=0.0.0.0:10000', 
-    '--start-child=thunar'
-  ])
-
-  xpraProcess.stdout.on('data', (data) => {
-    event.reply('xpra-status', data.toString())
-  })
-})
-
-app.whenReady().then(createMainWindow)
